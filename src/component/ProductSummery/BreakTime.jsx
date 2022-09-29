cdf543f1479c3a6553cc8a5e99de5b4a6599c744
coderpbt <yeasin800398@gmail.com>
2022-09-29 22:42:22 +0600

    Both responsive

--- src/component/ProductSummery/BreakTime.jsx
+++ src/component/ProductSummery/BreakTime.jsx
@@ -4,7 +4,7 @@ const BreakTime = ({item,handleBreakTime}) => {
 
   return (
     <>
-       <h6 onClick={()=>handleBreakTime(item)} className='cursor-pointer text-sm rounded-full font-bold w-9 h-9 bg-white leading-9'>{item.btime}</h6>
+       <h6 onClick={()=>handleBreakTime(item)} className='cursor-pointer mb-1 text-sm rounded-full font-bold w-9 h-9 bg-white leading-9'>{item.btime}</h6>
     </>
   );
 };
--- src/component/ProductSummery/ProductSummery.jsx
+++ src/component/ProductSummery/ProductSummery.jsx
@@ -42,17 +42,17 @@ const ProductSummery = ({cart}) => {
   
   return (
     <>
-      <div className='lg:w-[20%] w-[30%] bg-black-rgba min-h-screen'>
+       <div className='lg:w-[20%] w-[30%] bg-black-rgba min-h-screen'>
         <div className='sticky top-0'>
             <div className='flex-col md:flex lg:flex-row md:flex-row pt-6 pl-4'>
               <img className='w-[50px] rounded-full' src={man} alt="man" />
               <div className='ml-0 md:ml-3 text-left'>
-                <h2 className='font-bold'>Yeasin Arafat</h2>
+                <h2 className='font-bold text-xs md:text-base'>Yeasin Arafat</h2>
                 <p>Bangladesh</p>
               </div>
             </div>
 
-            <div className='flex justify-between px-4 py-6'>
+            <div className='flex-col md:flex lg:flex-row md:flex-row justify-between px-4 py-6'>
               <div>
                 <h2>60 <small>kg</small></h2>
                 <h6>Weight</h6>
@@ -68,8 +68,8 @@ const ProductSummery = ({cart}) => {
             </div>
 
             <div className='py-2'>
-               <h2 className='font-bold text-left pl-4'>Add A Break</h2>
-               <div className='px-4 flex justify-between items-center'>
+               <h2 className='font-bold text-left pl-4 text-xs md:text-base'>Add A Break</h2>
+               <div className='px-4 flex-col md:flex lg:flex-row md:flex-row justify-between items-center'>
                     {
                       breakTimeInfo.map((item,index) => 
                         <BreakTime
@@ -82,13 +82,13 @@ const ProductSummery = ({cart}) => {
                </div>
             </div>
 
-            <div className='py-2 px-4'>
-               <h2 className='font-bold text-left'>Exercise Details</h2>
-               <div className='flex justify-between items-center bg-slate-50 p-2 rounded-md my-3'>
+            <div className='py-2 px-0 md:px-4'>
+               <h2 className='font-bold text-center md:text-left text-xs md:text-base'>Exercise Details</h2>
+               <div className='flex-col md:flex lg:flex-row md:flex-row justify-between items-center bg-slate-50 p-2 rounded-md my-3'>
                   <h2 className='text-sm font-semibold'>Exercise Time</h2>
                   <span className='text-sm font-semibold'>{reduceTotal} Seconds</span>
                </div>
-               <div className='flex justify-between items-center bg-slate-50 p-2 rounded-md my-3'>
+               <div className='flex-col md:flex lg:flex-row md:flex-row justify-between items-center bg-slate-50 p-2 rounded-md my-3'>
                   <h2 className='text-sm font-semibold'>Break Time</h2>
                   <span className='text-sm font-semibold'>{times} Seconds</span>
                </div>
@@ -98,7 +98,7 @@ const ProductSummery = ({cart}) => {
             <a href='#' onClick={handelActivity} className='flex items-center bg-[#FF3030] rounded-xl p-2 justify-center text-white mx-2 mt-12'> <span className='mr-2'>Activity Completed </span>
             </a>
           </div>
-          </div>
+        </div>
     </>
   );
 };
