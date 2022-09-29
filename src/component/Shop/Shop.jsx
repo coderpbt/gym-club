a6f495f4f25666043831729791772a21e38cfcbd
coderpbt <yeasin800398@gmail.com>
2022-09-29 23:11:34 +0600

    final check

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -33,6 +33,7 @@ const Shop = () => {
             <img className='w-20' src={logo} alt="m-logo" />
             <h2 className='text-2xl'>DPI-GYM-CLUB</h2>
           </div>
+          <h1 className='text-left text-2xl font-bold mt-4'>Select Todays GYM</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
               {
                 products.map(product => <ShopTemplate product={product} key={product.id} handlerAddToCart={handlerAddToCart} />)
